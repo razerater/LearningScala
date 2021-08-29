93672da841e2d6aae788b2bf324ea6a1961a74c5
Raz Reed <raz@shipstone.com>
2021-08-28 18:13:07 -0700

    SBT working with Scala 2.13.6

---
+++ build.sbt
@@ -0,0 +1,5 @@
+lazy val root = (project in file("."))
+  .settings(
+    name := "Hello",
+    scalaVersion := "2.13.6"
+  )
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,5 +1,6 @@
 object Hello {
     def main(args: Array[String]) = {
-        println("Hello, world")
+        println("Hello, Raz")
     }
 }
+
